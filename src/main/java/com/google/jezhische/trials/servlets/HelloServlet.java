020f78cc439b10eea14e34411a40bf63f2fde8eb
jezhische <jezhische@gmail.com>
2017-09-13 05:10:37 +0300

    creating the simplest application is complete

--- src/main/java/com/google/jezhische/trials/servlets/HelloServlet.java
+++ src/main/java/com/google/jezhische/trials/servlets/HelloServlet.java
@@ -2,7 +2,7 @@ package com.google.jezhische.trials.servlets;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-public class HelloServlet {
+public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         try {
             httpServletResponse.getWriter().print("Hello from HelloServlet");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,5 +6,8 @@
     <body>
         <h1>Webapp Example - IdeaCommunityEdition + Maven + Tomcat</h1>
         <p>This is my template.</p>
+        <a href="https://www.w3.org/">W3C Website - href test</a>
+        <br/>
+        <h2><a href="servlet">Hello from servlet!</a></h2>
     </body>
 </html>
